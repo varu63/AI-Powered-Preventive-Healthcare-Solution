b0e83edcc2805499fd990e3caaed5f7bf13ef307
Varun Tomar <varunrajput8077@gmail.com>
2025-10-03 20:45:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,8 +136,9 @@ def get_float_value(form, key, default=0.0):
         return default
         
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+
 
 
 
